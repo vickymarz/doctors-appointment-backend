24e802a94aa6fb51f17d94643bc6d52406814427
iLynette <lynetteacholah@icloud.com>
2022-07-15 23:39:37 +0300

    Appontment new and create methods

--- app/controllers/v1/appointments_controller.rb
+++ app/controllers/v1/appointments_controller.rb
@@ -1,9 +1,34 @@
 class V1::AppointmentsController < ApplicationController
-  before_action :set_appointment, only: %i[ show edit update destroy ]
-
+before_action :set_appointment, only: %i[ show edit update destroy ]
   def index
     @appointments = current_user.appointments
   end
 
-  
+  def new
+    @appointment = Appointment.new
+  end
+
+  def create
+    @appointment = current_user.appointments.new(appointment_params)
+
+    if @appointment.save
+      render :create, status: :created
+    else
+      render json: @appointment.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @appointment.update(appointment_params)
+        format.html { redirect_to appointment_url(@appointment), notice: "Appointment was successfully updated." }
+        format.json { render :show, status: :ok, location: @appointment }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @appointment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+ 
 end

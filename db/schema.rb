582ff51c2459e698f9d0e5ac0437e570ce1719bc
vickymarz <okorojiebube2@gmail.com>
2022-07-18 03:42:55 -0600

    Create new user

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.0].define(version: 2022_07_18_074753) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-=======
-ActiveRecord::Schema[7.0].define(version: 2022_07_14_143902) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_18_092712) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,9 +38,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_14_143902) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "appointments", "doctors"
   add_foreign_key "appointments", "users"
->>>>>>> 58f985412700f12d1a72e1bc4cfc359f86160438
 end

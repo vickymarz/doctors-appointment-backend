365e9b6dc22c49578c39db8ae834f18caa772564
vickymarz <okorojiebube2@gmail.com>
2022-07-18 15:40:24 -0600

    Modify registeration controller actions

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,19 +1,20 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
 
+  private 
 
-  def respond_with(resource)
+  def respond_with(resource, opt = {})
 
-    register_success(resource) && return if resource.persisted?
+    register_success && return if resource.persisted?
 
     register_failed
   end
 
   def register_success(_resource)
-    render json: {message: 'Signed up sucessfully', status: 200}
+    render json: {message: 'Signed up sucessfully', user:current_user}, status: 200
   end
 
   def register_failed
-    render json: {message: 'Something went wrong'}
+    render json: {message: 'Something went wrong'}, status: :unprocessable_entity
   end  
 end

f9fa1364b3c778be0ba8215a184538b63ce36f69
vickymarz <okorojiebube2@gmail.com>
2022-07-19 14:29:38 -0600

    Create doctors

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -3,4 +3,20 @@ class Api::V1::DoctorsController < ApplicationController
     @doctors = Doctor.all
     render json: { doctors: @doctors}.to_json, status: :success
   end
+
+  def create
+    @doctor = Doctor.new(doctor_params)
+
+    if @doctor.save
+      render json: @doctor, status: :created
+    else
+      render json: @doctor.errors, status: :unprocessable_entity
+    end
+  end
+
+  def doctor_params
+    params.require(:doctor).permit(:name, :specialization, :charges)
+  end
+  
+  
 end

747293f2c0bcb0c1e11fd47c5ef1119ed2a13b94
vickymarz <okorojiebube2@gmail.com>
2022-07-19 02:26:59 -0600

    Add signin logic

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,25 +1,29 @@
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  
+  private
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def respond_with(_resource, _opts = {})
+    render json: {
+      message: 'You are logged In',
+      user: current_user
+    }, status: :ok
+  end
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def respond_to_on_destroy
+    log_out_success && return if current_user
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+    log_out_failure
+  end
 
-  # protected
+  def log_out_success
+    render json: {
+      message: 'You are logged out'
+    }, status: :ok
+  end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def log_out_failure
+    render json: {
+      message: 'something went wrong'
+    }, status: :unauthorized
+  end
 end

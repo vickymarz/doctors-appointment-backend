64d44bbbfe35fd5796224b6f926c4b545e7794b7
vickymarz <okorojiebube2@gmail.com>
2022-07-19 02:33:08 -0600

    Configure permitted parameter for login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,12 @@ class ApplicationController < ActionController::API
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password])
+
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+    user_params.permit(:name, :password)
   end
+  end
+
+  
+
 end

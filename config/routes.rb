cb6157c10bda6e96dc40e04c2c690e253f2a584b
iLynette <lynetteacholah@icloud.com>
2022-07-19 13:27:43 +0300

    Edit routes to test

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'doctors/index'
-  get 'appointments/index'
-  get 'users/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  namespace :v1 do 
+    get 'doctors/index'
+    get 'appointments/index'
+    delete 'appointments/:id', to: "appointments#destroy"
+    post 'appointments', to: "appointments#create"
+    get 'users/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end

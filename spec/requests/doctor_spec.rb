f2d35311153fe1f3b0d265cb19c83191f8d68f91
vickymarz <okorojiebube2@gmail.com>
2022-07-19 14:36:52 -0600

    Add request test to get create doctors

--- spec/requests/doctor_spec.rb
+++ spec/requests/doctor_spec.rb
@@ -1,6 +1,70 @@
 require 'rails_helper'
 
 RSpec.describe 'Doctor', type: :request do
+  describe 'POST /create' do
+    context 'with valid parameters' do
+   let!(:doctor_data) { FactoryBot.create(:doctor) }
+      before do
+        post '/api/v1/doctors', params:
+                          { doctor: {
+                            name: 'faith',
+                            specialization: 'coder',
+                            charges: doctor_data.charges
+                          } }
+      end
+
+      it 'returns a created status' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'with invalid parameters' do
+      before(:each) do
+        post '/api/doctors', params:
+                          { doctor: {
+                            name: '',
+                            specialization: 'coder',
+                            charges: 20
+                          } }
+      end
+
+      it 'returns a unprocessable entity status' do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+    context 'with invalid parameters' do
+      before(:each) do
+        post '/api/doctors', params:
+                          { doctor: {
+                            name: 'okoroji',
+                            specialization: '',
+                            charges: 20
+                          } }
+      end
+
+      it 'returns a unprocessable entity status' do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+    context 'with invalid parameters' do
+      before(:each) do
+        post '/api/doctors', params:
+                          { doctor: {
+                            name: 'okoroji',
+                            specialization: 'faith9@gmail.com',
+                            charges: ''
+                          } }
+      end
+
+      it 'returns a unprocessable entity status' do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+end
+
+
+RSpec.describe 'Doctor', type: :request do
   describe 'GET /index' do
     before do
       FactoryBot.create_list(:doctor, 10)

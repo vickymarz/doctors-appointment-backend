2b7b6bf1e2b48c9f06927bb4d18d1e7347ca7350
iLynette <lynetteacholah@icloud.com>
2022-07-20 16:23:53 +0300

    Swagger helper

--- spec/requests/users/sessions_spec.rb
+++ spec/requests/users/sessions_spec.rb
@@ -1,20 +1,17 @@
 require 'swagger_helper'
 
 RSpec.describe 'users/sessions', type: :request do
-
   path '/api/users/sign_in' do
-
     post('create session') do
       response(200, 'successful') do
-
-        consumes 'application/json'        
-        parameter name: :registration, in: :body, schema: {          
-        type: :object,          
-        properties: {          
-          name: { type: :string },            
-          password: { type: :string }
-        },          
-        required: %w[name password]  
+        consumes 'application/json'
+        parameter name: :registration, in: :body, schema: {
+          type: :object,
+          properties: {
+            name: { type: :string },
+            password: { type: :string }
+          },
+          required: %w[name password]
         }
 
         after do |example|
@@ -30,10 +27,8 @@ RSpec.describe 'users/sessions', type: :request do
   end
 
   path '/api/users/sign_out' do
-
     delete('delete session') do
       response(200, 'successful') do
-
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {

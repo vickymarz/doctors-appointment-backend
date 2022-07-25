62372f630c2eab5f4356ded0d5bf4c4ae6e8981d
iLynette <lynetteacholah@icloud.com>
2022-07-25 14:29:06 +0300

    Add doctors data in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Doctor.create!(name: 'Mercy', description: 'Hello there, nice to meet you', specialization: 'Ophthalmologist', charges: 100, photo: 'https://images.pexels.com/photos/5452201/pexels-photo-5452201.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')
+Doctor.create!(name: 'Kayline', description: 'Hello there, nice to meet you', specialization: 'Surgeon', charges: 100, photo: 'https://images.pexels.com/photos/6129507/pexels-photo-6129507.jpeg?auto=compress&cs=tinysrgb&w=800')
+Doctor.create!(name: 'Jess', description: 'Hello there, nice to meet you', specialization: 'Biologist', charges: 100, photo: 'https://images.pexels.com/photos/4173251/pexels-photo-4173251.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')
+Doctor.create!(name: 'Rose', description: 'Hello there, nice to meet you', specialization: 'Dentist', charges: 100, photo: 'https://images.pexels.com/photos/5452293/pexels-photo-5452293.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')
+Doctor.create!(name: 'Fabian', description: 'Hello there, nice to meet you', specialization: 'Therapy', charges: 100, photo: 'https://images.pexels.com/photos/5407206/pexels-photo-5407206.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')
+

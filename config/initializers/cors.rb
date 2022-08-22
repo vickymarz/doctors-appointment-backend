743174061b6e3248ffbe800e603b65fdf5c3bae2
vickymarz <okorojiebube2@gmail.com>
2022-08-22 16:11:29 +0100

    Add cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['https://doctorsappointmentapp.herokuapp.com', 'http://localhost:3000', 'http://localhost:3001']
+    origins ['https://alwayscare.herokuapp.com/', 'http://localhost:3000', 'http://localhost:3001']
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
